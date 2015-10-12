a29699322c7daf49618f325559e28b3f4b0fcdbe
Vincent Catalano <vincent@vincentcatalano.com>
2015-10-11 23:47:54 -0700

    Updating event interface types for latest Stripe version

--- pyramid_stripe/events.py
+++ pyramid_stripe/events.py
@@ -9,13 +9,68 @@ class Stripe(object):
         self.request = request
 
 
+@interface.implementer(interfaces.IAccount)
+class Account(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IAccountUpdated)
+class AccountUpdated(Account):
+    pass
+
+
+@interface.implementer(interfaces.IExternalAccountCreated)
+class ExternalAccountCreated(Account):
+    pass
+
+
+@interface.implementer(interfaces.IExternalAccountDeleted)
+class ExternalAccountDeleted(Account):
+    pass
+
+
+@interface.implementer(interfaces.IExternalAccountUpdated)
+class ExternalAccountUpdated(Account):
+    pass
+
+
+@interface.implementer(interfaces.IBalanceAvailable)
+class BalanceAvailable(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IBitcoin)
+class Bitcoin(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IBitcoinReceiverCreated)
+class BitcoinReceiverCreated(Bitcoin):
+    pass
+
+
+@interface.implementer(interfaces.IBitcoinReceiverFilled)
+class BitcoinReceiverFilled(Bitcoin):
+    pass
+
+
+@interface.implementer(interfaces.IBitcoinReceiverUpdated)
+class BitcoinReceiverUpdated(Bitcoin):
+    pass
+
+
+@interface.implementer(interfaces.IBitcoinReceiverTransactionCreated)
+class BitcoinReceiverTransactionCreated(Bitcoin):
+    pass
+
+
 @interface.implementer(interfaces.ICharge)
 class Charge(Stripe):
     pass
 
 
-@interface.implementer(interfaces.IChargeSucceeded)
-class ChargeSucceeded(Charge):
+@interface.implementer(interfaces.IChargeCaptured)
+class ChargeCaptured(Charge):
     pass
 
 
@@ -29,13 +84,68 @@ class ChargeRefunded(Charge):
     pass
 
 
-@interface.implementer(interfaces.IChargeDisputed)
-class ChargeDisputed(Charge):
+@interface.implementer(interfaces.IChargeSucceeded)
+class ChargeSucceeded(Charge):
+    pass
+
+
+@interface.implementer(interfaces.IChargeUpdated)
+class ChargeUpdated(Charge):
     pass
 
 
 @interface.implementer(interfaces.IChargeFailed)
 class ChargeFailed(Charge):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDispute)
+class ChargeDispute(Charge):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDisputeClosed)
+class ChargeDisputeClosed(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDisputeCreated)
+class ChargeDisputeCreated(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDisputeFundsReinstated)
+class ChargeDisputeFundsReinstated(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDisputeFundsWithdrawn)
+class ChargeDisputeFundsWithdrawn(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.IChargeDisputeUpdated)
+class ChargeDisputeUpdated(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.ICoupon)
+class Coupon(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.ICouponCreated)
+class CouponCreated(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.ICouponDeleted)
+class CouponDeleted(Stripe):
+    pass
+
+
+@interface.implementer(interfaces.ICouponUpdated)
+class CouponUpdated(Stripe):
     pass
 
 
@@ -49,20 +159,64 @@ class CustomerCreated(Customer):
     pass
 
 
+@interface.implementer(interfaces.ICustomerDeleted)
+class CustomerDeleted(Customer):
+    pass
+
+
 @interface.implementer(interfaces.ICustomerUpdated)
 class CustomerUpdated(Customer):
     pass
 
 
-@interface.implementer(interfaces.ICustomerDeleted)
-class CustomerDeleted(Customer):
+@interface.implementer(interfaces.ICustomerBankAccountDeleted)
+class CustomerBankAccountDeleted(Customer):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerDiscount)
+class CustomerDiscount(Customer):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerDiscountCreated)
+class CustomerDiscountCreated(CustomerDiscount):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerDiscountDeleted)
+class CustomerDiscountDeleted(CustomerDiscount):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerDiscountUpdated)
+class CustomerDiscountUpdated(CustomerDiscount):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerSource)
+class CustomerSource(Customer):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerSourceCreated)
+class CustomerSourceCreated(CustomerSource):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerSourceDeleted)
+class CustomerSourceDeleted(CustomerSource):
+    pass
+
+
+@interface.implementer(interfaces.ICustomerSourceUpdated)
+class CustomerSourceUpdated(CustomerSource):
     pass
 
 
 @interface.implementer(interfaces.ICustomerSubscription)
 class CustomerSubscription(Customer):
     pass
-
 
 @interface.implementer(interfaces.ICustomerSubscriptionCreated)
 class CustomerSubscriptionCreated(CustomerSubscription):
